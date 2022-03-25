623d5a5740a1b7fa096bee265c7c1269953665ff
KakaoPiraten <heinonen.simon@gmail.com>
2022-03-25 09:49:27 +0100

    Added "Hello"

--- YetAnotherCodeAlong/Program.cs
+++ YetAnotherCodeAlong/Program.cs
@@ -7,6 +7,8 @@ namespace YetAnotherCodeAlong
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+
         }
     }
 }
